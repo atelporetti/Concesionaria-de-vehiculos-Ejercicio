2df6eeac779059f2a7a4a1de9cee32b6fbea5fb6
Arturo Axel Poretti <69491395+atelporetti@users.noreply.github.com>
2021-01-18 18:29:23 -0300

    Add files via upload

---
+++ Java/Auto.java
@@ -0,0 +1,35 @@
+package onreadyfyMe;
+
+import java.text.DecimalFormat;
+
+public class Auto extends Vehiculo {
+	private String puertas;
+
+	public String getPuertas() {
+		return puertas;
+	}
+
+	public void setPuertas(String puertas) {
+		this.puertas = puertas;
+	}
+
+	public Auto(String marca, String modelo, double precio, String puertas) {
+		super(marca, modelo, precio);
+		this.puertas = puertas;
+	}
+
+	public Auto() {
+	}
+
+	public void mostrarTodosDatos() {
+		DecimalFormat formatea = new DecimalFormat("###,###.00");
+		System.out.println("Marca: " + getMarca() + " // Modelo: " + getModelo() + " // Puertas: " + getPuertas()
+				+ " // Precio: $" + formatea.format(getPrecio()));
+	}
+
+	@Override
+	public boolean esLujo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
